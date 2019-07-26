73bfe2d5eab6d2492d3cff20310d621fbe30b32b
sajjal <nairsajjal@gmail.com>
2019-07-26 17:17:40 +0530

    added a null pointer

--- pointer_demo.c
+++ pointer_demo.c
@@ -3,16 +3,19 @@
 
 int G=0; //global variable
 int main(int argc, char **argv)
-{
+{   int *nullp;
     static int s;
     int a ;
     int *p; //pointer variable for malloc bellow
     //obtain a block big enough for one int from the heap
     p = malloc(sizeof(int));
+
+    *nullp =0; 
     printf("&G = %p \n", (void *)&G);
     printf("&s = %p \n", (void *)&s);
     printf("&a = %p \n", (void *)&a);
     printf("&p = %p \n", (void *)&p);
     printf("p = %p \n", (void *)p);
     printf("main = %p \n", (void *)main);
+    printf("nullp = %p \n",&nullp);
 }
