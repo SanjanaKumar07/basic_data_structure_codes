8b8910da7456558524744ccb01648e591e3fc210
SanjanaKumar07 <56474199+SanjanaKumar07@users.noreply.github.com>
2019-10-13 11:41:03 +0530

    Update array_sum_demo.c

--- array_sum_demo.c
+++ array_sum_demo.c
@@ -3,36 +3,23 @@
 
 //this demo prints the sum of array A and also takes array inputs
 
-int sumArray(int num, int a[])
-    {
-        int j;
-        int sum;
-
-        sum=0;
-        for(j=0;j<num;j++){
-            sum+=a[j];}
-    
-     return sum;
-
-    }
 
 int main(){
-    int n, i,x,sum;
-    printf("enter the number of elements you want in the array \n");
+    int n, i,x,sum=0,a[100];
+    printf("enter the number of elements");
     scanf("%d", &n);
-
-    int a[n];
-
-    for(i=0;i<n;i++){
-        printf("Enter %d element of array:",(i+1));
+    for(i=0;i<n;i++)
+    {
+        printf("Enter %d elements of array:",(i+1));
         scanf("%d",&a[i]);
     }
 
-    printf("Till what element do you want the sum \n");
-    scanf("%d",&x);
-
-    sum = sumArray(x,a);
-    printf("The sum of the first %d elements is: %d \n", x, sum);
+    for(i=0;i<n;i++)
+    {
+        sum+=a[i];
+        
+    }
+    printf("The sum of the first elements is: %d \n", sum);
 
 }
 
